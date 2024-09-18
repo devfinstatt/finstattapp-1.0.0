e2bb215cfe4692ff743f584e0b06f0e35e0a03d5
FinstaTT Dev <dev@finstatt.com>
2024-09-18 15:11:44 -0400

    Change Feed to Home

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -32,7 +32,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::feed;
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
