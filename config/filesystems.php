72246b8860e5ec26c54bc2ca2ac37e9c29f2238e
devfinstatt <dev@finstatt.com>
2024-09-22 20:28:31 -0400

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -84,11 +84,12 @@ return [
 
         'do_spaces' => [
             'driver' => 's3',
-            'key' => '',
-            'secret' => '',
-            'endpoint' => '',
-            'region' => '',
-            'bucket' => '',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
             'visibility' => 'public',
         ],
 
